5f2e236a1ef840ac1bf79debca9e7136509564e3
thebestwj <wisestwang@163.com>
2019-11-19 16:55:13 +0800

    报表完成

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 import '../assets/css/global.css'
 Vue.use(VueRouter)
@@ -62,6 +63,10 @@ const routes = [
       {
         path: 'orders',
         component: Order
+      },
+      {
+        path: 'reports',
+        component: Report
       }
     ]
 
